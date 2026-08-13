5f78262bc3760f39b5b3d19e504454a57170dfa7
randytylor69 <Christophermaomao@gmail.com>
2026-08-13 14:13:38 -0400

    Added projection function to transform 3D vertices into 2D, taking consideration of their distance from the screen. Rather simple calculations, really. No trigonometry is used.

--- main.cpp
+++ main.cpp
@@ -12,21 +12,24 @@ using namespace std;
 
 
 Engine engine;
+string symbols = "abcdefghijkl";
 
 /* defining the 3D cube, which consists of triangles & vertices */
 struct Vec3 {
     float x, y, z;
+    Vec3() {}
     Vec3(const float &xi, const float &yi, const float &zi)
 	: x(xi), y(yi), z(zi) {}
 };
 
 struct Vec2 {
     float x, y;
+    Vec2() {}
     Vec2(const float &xi, const float &yi)
 	: x(xi), y(yi) {}
 };
 
-vector<Vec3> vertices {
+vector<Vec3> cubeVertices {
     Vec3(-1, -1, -1), // 0
     Vec3(-1,  1, -1), // 1
     Vec3( 1,  1, -1), // 2
@@ -37,7 +40,7 @@ vector<Vec3> vertices {
     Vec3(-1,  1,  1), // 7
 };
 
-vector<vector<int>> tringles = {
+vector<vector<int>> cubeTriangles = {
     /* each int represents a vertex, 
      * indexed exactly as the members 
      * of "vertices" above */
@@ -62,10 +65,16 @@ vector<vector<int>> tringles = {
     {1, 4, 2}
 };
 
-void drawCube()
+Vec2 project3DTo2D(Vec3 v)
 {
-
+    /* note that not only should the x & y coordinates be scaled by z, 
+     * we should also push the cube into the middle of the screen */
+    return Vec2(
+	round(v.x/v.z + TERM_COLS/2), 
+	round(v.y/v.z + TERM_ROWS/2)
+    );
 }
+
 
 
 
@@ -90,13 +99,13 @@ void drawScanLine(const int &y, const int &x0, const int &x1, char symbol)
 }
 
 
-void drawFlatBottomTriangle(Vec2 top, Vec2 bottom1, Vec2 bottom2)
+void drawFlatBottomTriangle(Vec2 top, Vec2 bottom1, Vec2 bottom2, char symbol)
 {
     Vec2 pStart = top;
     Vec2 pEnd = top;
     while (pStart.y != bottom1.y) {
 
-	drawScanLine(pStart.y, pStart.x, pEnd.x, '*');
+	drawScanLine(pStart.y, pStart.x, pEnd.x, symbol);
 
 	/* modify pStart */
 	pStart.y += 1;
@@ -106,16 +115,16 @@ void drawFlatBottomTriangle(Vec2 top, Vec2 bottom1, Vec2 bottom2)
 	pEnd.y += 1;
 	pEnd.x += (bottom2.x - top.x) / (bottom2.y - top.y);
     }
-    drawScanLine(pStart.y, pStart.x, pEnd.x, '*');
+    drawScanLine(pStart.y, pStart.x, pEnd.x, symbol);
 }
 
-void drawFlatTopTriangle(Vec2 bottom, Vec2 top1, Vec2 top2)
+void drawFlatTopTriangle(Vec2 bottom, Vec2 top1, Vec2 top2, char symbol)
 {
     Vec2 pStart = bottom;
     Vec2 pEnd = bottom;
     while (pStart.y != top1.y) {
 
-	drawScanLine(pStart.y, pStart.x, pEnd.x, '*');
+	drawScanLine(pStart.y, pStart.x, pEnd.x, symbol);
 
 	/* modify pStart */
 	pStart.y -= 1;
@@ -125,10 +134,20 @@ void drawFlatTopTriangle(Vec2 bottom, Vec2 top1, Vec2 top2)
 	pEnd.y -= 1;
 	pEnd.x -= (bottom.x - top2.x) / (bottom.y - top2.y);
     }
+    drawScanLine(pStart.y, pStart.x, pEnd.x, symbol);
 }
 
-void drawTriangle(Vec2 v0, Vec2 v1, Vec2 v2)
+void drawTriangle(Vec2 inputV0, Vec2 inputV1, Vec2 inputV2, char symbol)
 {
+    /* first sort the vertices by ascending Y */
+    vector<Vec2> sortedVertices = {inputV0, inputV1, inputV2};
+    sort(sortedVertices.begin(), sortedVertices.end(), 
+	[](const auto &L, const auto &R) {return L.y < R.y;}
+    );
+    Vec2 v0 = sortedVertices[0];
+    Vec2 v1 = sortedVertices[1];
+    Vec2 v2 = sortedVertices[2];
+
     /* compute the middle point using right angled triangles */
     Vec2 midpoint = {0, 0};
     midpoint.y = v1.y;
@@ -136,43 +155,64 @@ void drawTriangle(Vec2 v0, Vec2 v1, Vec2 v2)
     midpoint.x = v0.x + (v2.x - v0.x) / (v2.y - v0.y) * (v1.y - v0.y);
 
     /* split the triangle into 2 and use both functions to draw it */
-    drawFlatBottomTriangle(v0, midpoint, v1);
-    drawFlatTopTriangle(v2, midpoint, v1);
+    drawFlatBottomTriangle(v0, midpoint, v1, symbol);
+    drawFlatTopTriangle(v2, midpoint, v1, symbol);
 }
 
+
+void drawCube()
+{	
+    int symbolIndex = 0;
+
+    for (auto triangle : cubeTriangles) 
+    {
+	Vec3 transformed3DVertices[3]; // create a copy of current triangle's vertices
+	for (int i=0; i<3; i++)
+	{
+	    transformed3DVertices[i] = cubeVertices[triangle[i]];
+
+	    /* Transform each one of the 3 vertices */
+
+	    /* 1. Push it into the screen */
+	    transformed3DVertices[i].z += 10;
+
+	    /* 2. Scale it, but only the x and y, not z. And not by the same factor,
+	     *    because a character's height > its width */
+	    int scaleFactor {80};
+	    transformed3DVertices[i].y *= scaleFactor;
+	    transformed3DVertices[i].x *= scaleFactor * 3;
+	    
+	}
+
+	/* project 3D to 2D */
+	Vec2 transformed2DVertices[3];
+	for (int i=0; i<3; i++) 
+	    transformed2DVertices[i] = project3DTo2D(transformed3DVertices[i]);
+	/* draw transformed triangle */
+	drawTriangle(
+	    transformed2DVertices[0], 
+	    transformed2DVertices[1], 
+	    transformed2DVertices[2],
+	    symbols[symbolIndex]
+	);
+
+	symbolIndex++;
+    }
+}
 
 int main()
 {
     engine.setCanonicalAndCursor(0);
     engine.clearScreen();
-    Vec2 v0 = {15, 5};
-    Vec2 v1 = {50, 15};
-    Vec2 v2 = {25, 30};
-
-    drawTriangle(v0, v1, v2);
-    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-
+    
+    drawCube();
+    // Vec2 v0 {10, 9};
+    // Vec2 v1 {30, 9};
+    // Vec2 v2 {15, 25};
+    // drawTriangle(v0, v1, v2);
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     engine.setCanonicalAndCursor(1);
     
 }
 
 
-	//    array<array<char, TERM_COLS>, TERM_ROWS> screen {};
-	//    fill(&screen[0][0], &screen[0][0] + sizeof(screen), ' ');
-	//    int screenBuffer[TERM_ROWS * TERM_COLS] = {};
-	//
-	//    array<char, 4> foo= {'q', 'w', 'e', 'r'};
-	//    int count = 0;
-	//    while (true) {
-	// if (count == 5) break;
-	// // update
-	// engine.clearScreen();
-	// screen[0][0] = foo[count];
-	// for (const auto &row: screen)
-	// {
-	//     for (const auto &col : row) print("{}", col);
-	//     print("\n");
-	// }
-	// count++;
-	// engine.delay(1000000, 0);
-	//    }
